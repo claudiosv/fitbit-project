5e511b5b725a13ae547cabb72cbb144a6301fefb
Claudio Spiess <claudiosv@users.noreply.github.com>
2017-11-28 02:10:09 -0500

    Fix UI bugs

--- src/edu/cofc/Main.java
+++ src/edu/cofc/Main.java
@@ -1,7 +1,4 @@
 package edu.cofc;
-//TODO: Timer Fix button stretch
-//TODO: Timer Add Stop Button (set remaingTime 0)
-//TODO: Step Counter add steps on save screen
 
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -33,7 +30,11 @@ public class Main extends Application {
     public Scene scene;
     public Pane test;
     public Button btnTest;
-    BorderPane border;
+    private BorderPane border;
+
+    public static void main(String args[]) {
+        launch(args);
+    }
 
     public GridPane addSleepTracker() {
         GridPane grid = new GridPane();
@@ -177,6 +178,12 @@ public class Main extends Application {
         grid.setVgap(5);
         grid.setPadding(new Insets(0, 5, 0, 5));
 
+        Label category = new Label(String.format("%.0f bpm", heartHistory.heartMonitor.bpmProperty().get()));
+        category.setWrapText(true);
+        category.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+        category.setMaxWidth(113);
+        category.setVisible(true);
+
         // Category in column 2, row 1
         Button btnSave = new Button();
         Image imageSave = new Image(getClass().getResourceAsStream("003-save.png"));
@@ -191,9 +198,11 @@ public class Main extends Application {
             public void handle(ActionEvent event) {
                 heartHistory.addHeartRate(heartHistory.heartMonitor.saveHeartRate());
                 heartHistory.heartMonitor.resetHeart();
+                category.setText("Saved successfully");
             }
         });
-        grid.add(btnSave, 2, 2);
+        grid.add(category, 0, 0);
+        grid.add(btnSave, 0, 1);
         return grid;
 
     }
@@ -255,7 +264,7 @@ public class Main extends Application {
 
         // Category in column 2, row 1
 
-
+        Button btnStop = new Button();
         Label category = new Label("100 secs");
         Slider slider = new Slider();
         slider.setMin(1);
@@ -282,22 +291,39 @@ public class Main extends Application {
         Image imageDecline = new Image(getClass().getResourceAsStream("002-play-button.png"));
         ImageView imageView = new ImageView(imageDecline);
         imageView.setFitHeight(16);
-        imageView.setFitWidth(32);
+        imageView.setFitWidth(16);
         btn1.setGraphic(imageView);
         timer.remainingTimeProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue.intValue() <= 0) {
-                Platform.runLater(() -> border.setCenter(addFinishedTimer()));
+                Platform.runLater(() -> {border.setCenter(addFinishedTimer());
+                btnStop.setVisible(false);
+                slider.setVisible(true);});
             } else {
                 Platform.runLater(() -> category.setText(String.format("%d secs", newValue.intValue())));
             }
         });
-        btn1.setOnAction(event ->
-                timer.startTimer((int) slider.getValue())
-
+        btn1.setOnAction(event -> {
+                    timer.startTimer((int) slider.getValue());
+                    slider.setVisible(false);
+                    btnStop.setVisible(true);
+                }
         );
+
+
+        Image imageStop = new Image(getClass().getResourceAsStream("002-stop.png"));
+        ImageView imageViewStop = new ImageView(imageStop);
+        imageViewStop.setFitHeight(16);
+        imageViewStop.setFitWidth(16);
+        btnStop.setGraphic(imageViewStop);
+
+        btnStop.setOnAction(event -> {
+                    timer.remainingTimeProperty().setValue(0);
+                }
+        );
+        btnStop.setVisible(false);
         grid.add(slider, 0, 1);
         grid.add(btn1, 0, 2);
-
+        grid.add(btnStop, 0, 2);
         grid.add(category, 0, 0, 3, 1);
 
 
@@ -305,6 +331,7 @@ public class Main extends Application {
     }
 
     public GridPane addFinishedTimer() {
+        btnTest.setVisible(true);
         GridPane grid = new GridPane();
         grid.setHgap(5);
         grid.setVgap(5);
@@ -364,7 +391,7 @@ public class Main extends Application {
         grid.setPadding(new Insets(0, 5, 0, 5));
 
         // Category in column 2, row 1
-        Label category = new Label();
+        Label category = new Label("0 steps");
         category.setWrapText(true);
 
         category.setFont(Font.font("Arial", FontWeight.BOLD, 15));
@@ -428,15 +455,22 @@ public class Main extends Application {
         imageViewSave.setFitWidth(16);
         btnSave.setGraphic(imageViewSave);
 
+        Label category = new Label(String.format("%d steps", stepHistory.stepCounter.getNumSteps()));
+        category.setMaxWidth(113);
+        category.setWrapText(true);
+
+        category.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+
         btnSave.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
                 stepHistory.addDailyCount(stepHistory.stepCounter.saveDailyCount());
-                //TODO: show label for saved successfully
+                category.setText("Saved successfully");
             }
         });
-        grid.add(btnSave, 2, 2);
+        grid.add(category, 0, 0);
+        grid.add(btnSave, 0, 1);
         return grid;
     }
 
@@ -630,162 +664,5 @@ public class Main extends Application {
             System.exit(0);
         });
         primaryStage.show();
-    }
-
-    public static void main(String args[]) {
-        launch(args);
-
-         /* How the step counter works: (This will be activated by switching to step counter screen and pressing start)
-            - Starts the step counter (constantly reading from the accelerometer)
-            - Thread will be created to update the step count on screen
-            - Stops the counter when the stop button is pressed
-            - When saved is pressed it will save the daily step count
-         */
-
-        /*
-        if(stepHistory.stepCounter.startCounter()) {
-            Runnable testingThread = new Runnable() {
-                public void run() {
-                    try {
-                        //This will be a while(onStepCountScreen) {} using sleep for testing purposes
-                        //Update the text on screen
-                        Thread.sleep(2000);
-                        System.out.printf("Step Count: %d\n", stepHistory.stepCounter.readStepCount());
-
-                    } catch (InterruptedException e) {
-                        //thread interrupted display error
-                    }
-
-                    //Stop button pressed
-                    stepHistory.stepCounter.stopCounter();
-
-                    //Saves button pressed, saves and resets
-                    stepHistory.addDailyCount(stepHistory.stepCounter.saveDailyCount());
-                    stepHistory.stepCounter.resetSteps();
-                }
-            };
-
-            Thread testThread = new Thread(testingThread);
-            testThread.start();
-        }
-        */
-
-
-        /*
-           How the Timer works: (activated by switching to the timer screen and pressing start)
-             - Inputs the desired seconds from the UI
-             - Creates thread to count down
-             - Stops when time has been reached
-         */
-
-        /*
-        timer.startTimer(100); //1 minute 40 seconds for testing
-
-         */
-
-        /* Heart Monitor
-            - Heart.startHeart will be called from pressing a button
-            - A thread should be created to constantly be reading the Heart.getHeartRate()
-            - Use this to update the UI
-            - Heart.stopHeart and Heart.resetHeart will be used to stop the heart monitor from buttons
-         */
-
-        /*
-        if(heartHistory.heartMonitor.startHeart()) {
-            Runnable testingThread = new Runnable() {
-                public void run() {
-                    try {
-                        for(int i = 0; i < 40; i++) {
-                            //Read for twenty seconds
-                            System.out.printf("Heart Rate (BPM): %f\n", heartHistory.heartMonitor.readHeartRate());
-
-                            Thread.sleep(500);
-                        }
-                    } catch (InterruptedException e) {
-                        //thread interrupted display error
-                    }
-
-                    //Stop button pressed
-                    heartHistory.heartMonitor.stopHeart();
-
-                    //Saves button pressed, saves and resets
-                    heartHistory.addHeartRate(heartHistory.heartMonitor.saveHeartRate());
-                    heartHistory.heartMonitor.resetHeart();
-                }
-            };
-
-            Thread testThread = new Thread(testingThread);
-            testThread.start();
-        }
-        */
-
-        /* Sync, Companion and User
-            - Sync should be called at the opening of the app
-            - This will set up the user (just fills in values)
-            - The sync process takes 2.5 seconds (just uses a sleep so this can change) so UI can show a 2.5 progress bar for the sync process?
-         */
-
-        /*
-        if(sync.addCompanion()) {
-            Runnable syncRunnable = new Runnable() {
-                public void run() {
-                    try {
-                        System.out.println("Starting the sync process");
-                        sync.syncData("Empty Data to send");
-
-                        Thread.sleep(2550);
-                        System.out.println("Sync process completed");
-
-                        System.out.println("user: " + user);
-                        System.out.printf("Username: %s\n", user.username);
-                        System.out.printf("Gender: %s\n", user.getGender());
-                        System.out.printf("Birthday: %s\n", user.getBirthday());
-                        System.out.printf("Height: %d\n", user.getHeight());
-                        System.out.printf("Weight: %d\n", user.getWeight());
-                    } catch (InterruptedException e) {
-                        //thread interrupted, display sync error
-                    }
-                }
-            };
-
-            Thread syncThread = new Thread(syncRunnable);
-            syncThread.start();
-        }
-        */
-
-        /* Sleep Tracker */
-
-        /*
-        if(sleepHistory.sleepTracker.startSleepTracker()) {
-            Runnable testRun = new Runnable() {
-                public void run() {
-                    try {
-                        while (!sleepHistory.sleepTracker.isSleeping()) {
-                            System.out.println("waiting to fall asleep");
-                            Thread.sleep(500);
-                        }
-                        while(sleepHistory.sleepTracker.isSleeping()) {
-                            System.out.println("waiting to wake up");
-                            Thread.sleep(500);
-                        }
-
-                        System.out.printf("Saving sleep times\n");
-                        sleepHistory.addSleepTimes(sleepHistory.sleepTracker.saveNightsSleep());
-
-                        NightsSleep lastNight = sleepHistory.getSleepTimes(0);
-                        System.out.printf("Fell Asleep At: %s\n", lastNight.getSleepTime());
-                        System.out.printf("Woke Up At: %s\n", lastNight.getWakeTime());
-                        System.out.printf("Seconds Asleep: %d\n", lastNight.secondsAsleep());
-                    }
-                    catch (InterruptedException e) {
-                        //thread interrupted, display error
-                    }
-                }
-            };
-
-            Thread testThread = new Thread(testRun);
-            testThread.start();
-        }
-        */
     }
 }
